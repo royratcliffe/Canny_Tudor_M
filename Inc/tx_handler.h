4373e6031ff99f0d0b149464ee4b51a1a3b95f83
Roy Ratcliffe <roy@ratcliffe.me>
2023-03-10 19:28:00 +0000

    feat: vTx{Message,Stream}BufferHandler

--- Inc/tx_handler.h
+++ Inc/tx_handler.h
@@ -36,5 +36,12 @@
  * successfully-transmitted bytes, by design. The caller expects the handler to
  * block until the sender transmits all data bytes. The caller task only wants
  * to transfer the data. The handler always runs within the context of a task.
+ *
+ * The transmit handler must not retain the data, including by reference.
+ * The handler should parse and retain only copies of the data as necessary.
  */
 typedef void (*TxHandler_t)(void *pvSender, const void *pvTxData, size_t xDataLengthBytes);
+
+void vTxMessageBufferHandler(void *pvMessageBuffer, const void *pvTxData, size_t xDataLengthBytes);
+
+void vTxStreamBufferHandler(void *pvStreamBuffer, const void *pvTxData, size_t xDataLengthBytes);
